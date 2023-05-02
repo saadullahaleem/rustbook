e29a685a5f400682375f80643842bb2222350ed9
Saadullah Aleem <aleemsaadullah@gmail.com>
2023-05-03 04:01:55 +0500

    fun with error handling, also, a proper game now

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,37 +1,55 @@
 use std::io;
+use std::cmp::Ordering;
 use rand::Rng;
 
 fn main() {
-    let mut guess = String::new();
     let mut lives = 4;
+    let secret_number = rand::thread_rng().gen_range(1..=100);
 
     while lives > 0 {
 
-        let secret_number = rand::thread_rng().gen_range(1..=100);
-
+        println!("You have {lives} lives left!");
         println!("Guess the number or press q to quit");
         println!("Please input your guess.");
+
+        let mut guess = String::new();
 
         io::stdin()
             .read_line(&mut guess)
             .expect("Failed to read line");
 
-        if guess.trim() == "q" {
-            println!("You quit the game. SAD!");
-            break;
-        }
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                if guess.trim() == "q" {
+                    println!("You quit the game!");
+                    break;
+                } else {
+                    println!("Please enter a number!");
+                    println!();
+                    continue;
+                }
+            }
+        };
 
-        if secret_number == guess.trim().parse::<u32>().unwrap() {
-            println!("You guessed the correct number. You win!");
-        } else {
-            lives -= 1;
-            guess = String::new();
-            println!("The correct number is {secret_number}. You did not guess it!");
-            println!("You have {lives} lives left!");
+        match guess.cmp(&secret_number) {
+            Ordering::Less => {
+                println!("Too small!");
+                lives -= 1;
+            },
+            Ordering::Greater => {
+                println!("Too big!");
+                lives -= 1;
+            },
+            Ordering::Equal => {
+                println!("You guessed the correct number. You win!");
+                break;
+            },
         }
 
         if lives == 0 {
             println!("You have no more lives left! You lose!");
+            println!("The secret number was: {}", secret_number);
         }
         println!();
     }
